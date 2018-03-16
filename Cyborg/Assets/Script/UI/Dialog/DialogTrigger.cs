41627c20be87ee88545656bf870617a38665baba
Kory Hunter <kouriiraiko@gmail.com>
2018-03-16 11:41:55 -0400

    Dialogue Zones and Pause Update
    DialogTrigger now has two settings: IMMEDIATE and ON_PLAYER_ENTER.
    
    IMMEDIATE DialogTriggers will instantly attempt to send their dialogue to the UI, such as on the main menu.
    
    ON_PLAYER_ENTER DialogTriggers will only send their dialogue to the UI when a player enters the corresponding BoxCollider.
    
    DialogManager now has references to all of the subcomponents of "Dialog" in the canvas.  This allows it to enable or disable them from view, using the new ToggleAllDialogUI(bool) method. It also has NextButton which will force DisplayNextSentence() when clicked.
    
    UI is hidden by default now, and the canvas prefab has been updated for all levels so that it only displays when dialogue is sent to the UI, and automatically hides when there is no dialogue left to read.
    
    PAUSE_GAME has been updated to be made of two component booleans: PauseButton and DialogPause. The game will pause itself if EITHER the player pauses, or the dialogue system pauses. This distinction also prevents the player from unpausing the game by pressing the pause button when the dialogue system has forcefully paused the game!

--- Cyborg/Assets/Script/UI/Dialog/DialogTrigger.cs
+++ Cyborg/Assets/Script/UI/Dialog/DialogTrigger.cs
@@ -1,14 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class DialogTrigger : MonoBehaviour {
 
+    private enum TRIGGER_TYPE
+    {
+        IMMEDIATE,
+        ON_PLAYER_ENTER
+    };
+
+
+    //All DialogTriggers
+    [SerializeField]
+    private TRIGGER_TYPE triggerType = TRIGGER_TYPE.IMMEDIATE;
     public Dialog dialogue;
+
+    //Only for ON_PLAYER_ENTER DialogTriggers
+    Collider2D dialogCollider;
+    [SerializeField]
+    GameObject dialogUI;
+
 
     private void Awake()
     {
         StartCoroutine(DelayConversaion());
+    }
+
+    private void Start()
+    {
+        if (triggerType == TRIGGER_TYPE.ON_PLAYER_ENTER)
+        {
+            dialogCollider = this.gameObject.GetComponent<BoxCollider2D>();
+        }
     }
 
     public void TriggerDialogue()
@@ -25,8 +50,29 @@ public class DialogTrigger : MonoBehaviour {
     IEnumerator DelayConversaion()
     {
        
-         yield return 5;   
+         yield return 5;
 
-        TriggerDialogue();
+        if (triggerType == TRIGGER_TYPE.IMMEDIATE)
+        {
+            TriggerDialogue();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (triggerType == TRIGGER_TYPE.ON_PLAYER_ENTER)
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                if (!dialogUI.activeSelf)
+                {
+                    dialogUI.SetActive(true);
+                }
+                Debug.Log("Player Entered dialog zone.");
+                dialogCollider.enabled = false;
+
+                TriggerDialogue();
+            }
+        }
     }
 }
