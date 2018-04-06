fefa61bee836637e1f4b95455a3281d1a80c7300
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-04-06 13:14:44 -0400

    Added end game scene
    Added trigger to sunny door at the end of level 4.  Trigger takes you to end game screen.  The end game screen then takes you back to the main menu.

---
+++ Cyborg/Assets/GoToEndGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToEndGame : MonoBehaviour {
+    private bool end = true;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (end)
+        {
+            Debug.Log("End game");
+            Invoke("EndGame", 2);
+            end = false;
+        }
+       
+    }
+
+    private void EndGame()
+    {
+        SceneManager.LoadScene("EndGameScreen");
+    }
+}
---
+++ Cyborg/Assets/ReturnToMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour {
+    void Start()
+    {
+        Invoke("GoToMainMenu", 5);
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
