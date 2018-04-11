5f32948c5dc9d76595da2f627232787ecc1218be
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-04-11 00:35:57 -0400

    Added Starting Dialog for Level 1

---
+++ Cyborg/Assets/RenableColliders.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenableColliders : MonoBehaviour {
+    public GameObject colliderHolder;
+    public AudioSource alarm;
+    private bool firstCollide = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        colliderHolder.SetActive(true);
+        if (firstCollide)
+        {
+            alarm.Play();
+            firstCollide = false;
+            Invoke("StopAlarm", 5);
+        }
+
+    }
+
+    private void StopAlarm()
+    {
+        alarm.mute = true;
+    }
+}
